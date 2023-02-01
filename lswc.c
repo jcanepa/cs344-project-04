2eecf52e3e7b97a904766b51d4cebac3f4261baf
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-01 11:16:42 -0800

    Add comments of rough game plan.

--- lswc.c
+++ lswc.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>  // printf, perror
+#include <unistd.h> // exit
+#include <fcntl.h>  // fork, execlp, close
 
 int main(void)
 {
+    // Syscalls needed:
     // fork()
     // exec()
     // pipe()
