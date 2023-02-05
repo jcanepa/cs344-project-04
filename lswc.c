f41a3f136bc1f4436b512cd5741d6b729fb512e3
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-05 12:09:21 -0800

    Add .gitignore rule.

--- lswc.c
+++ lswc.c
@@ -2,11 +2,44 @@
 #include <unistd.h> // exit
 #include <fcntl.h>  // fork, execlp, close
 
-int main(void)
+int main(int argc, char const *argv[])
 {
-    // Syscalls needed:
-    // fork()
-    // exec()
-    // pipe()
-    // dup2()
+    // parse the command line
+    if (argc > 1)
+        printf("%s\n", argv[1]); // user provided a dir
+
+    int pipe_fd[2]; // 0 ← in, 1 ← out
+    pipe(pipe_fd);
+
+    int fd = open("foo.out", O_CREAT | O_WRONLY, 0644); // r/w permissions
+
+    pid_t pid = fork();
+
+    // handle fork error
+    if (pid == -1)
+    {
+        perror("fork");
+        return 1;
+    }
+
+    // child process
+    if (pid == 0)
+    {
+        // read end into stdin
+        dup2(pipe_fd[0], 0);
+        // close write end
+        close(pipe_fd[1]);
+
+        // do wc
+        // execlp("/usr/bin");
+
+        perror("execlp");
+        exit(0);
+    }
+
+    // write();
+
+    // wait(NULL);
+
+    close(fd);
 }
